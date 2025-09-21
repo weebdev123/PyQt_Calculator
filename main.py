427deb65c02fa066c6fc237e34b989849b98d3d5
weebdev123 <166636609+weebdev123@users.noreply.github.com>
2025-09-21 22:44:28 +0530

    Resizing

--- main.py
+++ main.py
@@ -86,6 +86,6 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.resize(800, 600)
+    window.resize(300, 300)
     window.show()
     sys.exit(app.exec_())
